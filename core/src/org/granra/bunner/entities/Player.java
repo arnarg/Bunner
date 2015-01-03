6a78bc3847d063127ab0a2ba960e7a8ecdce14a9
Arnar Gauti Ingason <granra@gmail.com>
2015-01-03 01:13:55 +0000

    Adding Player class

---
+++ core/src/org/granra/bunner/entities/Player.java
@@ -0,0 +1,31 @@
+package org.granra.bunner.entities;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by arnar on 1/3/15.
+ */
+public class Player {
+
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+    private int width;
+    private int height;
+
+    private int nrOfJumps;
+
+    public Player(float x, float y, int width, int height) {
+
+        this.width = width;
+        this.height = height;
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        acceleration = new Vector2(0, 0);
+
+        nrOfJumps = 0;
+
+    }
+
+}
