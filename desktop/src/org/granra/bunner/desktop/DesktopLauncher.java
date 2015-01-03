3c01008d9ad241cc13bdf169ad370edd1ea8cf8b
Arnar Gauti Ingason <granra@gmail.com>
2015-01-03 00:55:04 +0000

    Adding libgdx project

---
+++ desktop/src/org/granra/bunner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.granra.bunner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.granra.bunner.Bunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bunner(), config);
+	}
+}
